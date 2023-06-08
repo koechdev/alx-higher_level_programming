30ae4c723c3feccf9b7820149562da6503de1a34
KIRWAMC <kirwamarum@gmail.com>
2023-06-08 13:01:20 -0700

    update5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,11 @@
-[200~#!/usr/bin/python3
-        from variable_load_5 import a
+#!/usr/bin/python3
+from variable_load_5 import a
 
 
-        def find_var():
-            print(a)
+def find_var():
+    print(a)
 
 
-            if __name__ == "__main__":
-                find_var()
+if __name__ == "__main__":
+    find_var()
+
