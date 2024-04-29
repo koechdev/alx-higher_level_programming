0220334e63d7a42fbcb1ca0ebeb973b393d88791
koechdev <kirwamarum@gmail.com>
2024-04-29 10:25:56 -0700

    update

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,37 +1,36 @@
-#!/usr/bin/python3
-"""List relationship"""
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from relationship_state import Base, State
-from relationship_city import City
-import sys
-
-
-def fetch_states_cities():
-    """
-    Lists all States and City objects
-    in the database
-    """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        username, password, database), pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-    session = Session()
-
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
-
-    session.commit()
-    session.close()
-
+from sqlalchemy.orm import sessionmaker
+from relationship_city import City, State
 
 if __name__ == "__main__":
-    fetch_states_cities()
+    # Get arguments from command line
+    username, password, database_name = (
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3],
+    )
+
+    # Create connection string
+    connection_string = f"mysql+pymysql://{username}:{password}@localhost:3306/{database_name}"
+
+    # Create database engine
+    engine = create_engine(connection_string)
+
+    # Create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query City objects with state relationship
+    cities = (
+        session.query(City)
+        .join(City.state)
+        .order_by(City.id)
+        .all()
+    )
+
+    # Print results
+    for city in cities:
+        print(f"{city.id}: {city.name} ({city.state.name})")
+
+    # Close session
+    session.close()
